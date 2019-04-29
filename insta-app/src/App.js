412df6f03e60d041706e62a27e926d0f77dfe9d5
Ming Liu <mliu@macpro.home>
2019-04-29 13:16:34 -0400

    start import

--- insta-app/src/App.js
+++ insta-app/src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
-
-
-
+import SearchBar from './components/SearchBar/SearchBar'
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       posts : dummyData,
+      searchField: "",
 
     }
+  }
+
+  onChange_SearchField = event => {
+
+  }
+
+  onClick_SearchButton = event => {
+
   }
 
   render() {
     return (
     <div className="App">
       <header className="App-header">
+        <SearchBar onChange = {this.onChange_SearchField} onClick = {this.onClick_SearchButton} />
        
       </header>
     </div>
---
+++ insta-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SearchBar.css/';
+
+
+const SearchBar = props => {
+    return (
+        <div>
+            <div><span className="logo"></span></div>
+        </div>
+    )
+}
+
+export default SearchBar;
